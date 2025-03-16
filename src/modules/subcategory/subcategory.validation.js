d8a8742ac9568e6960d0003c1379471e4807ca5c
Nehal Gamal <nehalgamal093@gmail.com>
2025-03-17 00:09:26 +0200

    subcat image

--- src/modules/subcategory/subcategory.validation.js
+++ src/modules/subcategory/subcategory.validation.js
@@ -2,6 +2,7 @@ import Joi from "joi";
 
 export const createSubCategorySchema = Joi.object({
   name: Joi.string().min(2).max(20).required(),
+  category: Joi.string().min(2).max(50).required(),
 });
 
 // export const getCategorySchema = Joi.object({
